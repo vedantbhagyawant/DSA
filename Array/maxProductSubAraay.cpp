267d397ee33e6dbb17206c3e7a18518c0428921c
Vedant Bhagyawant <maharaja@e.gov>
2024-07-17 09:00:41 +0530

    njjn

---
+++ Array/maxProductSubAraay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProductSubArray(vector <int> &nums){
+    int prod1 = nums[0]; 
+    int prod2 = nums[0];
+    int  result = nums[0];
+
+    for(int i = 1; i < nums.size(); i++){
+      int  temp = max({nums[i], prod1 * nums[i],prod2 * nums[i]});
+      prod2 = min({nums[i], prod1 * nums[i], prod2 * nums[i]});
+      prod1 = temp;
+    }
+    result = max(result,prod1);
+    return result;
+
+}
+
+int main(){
+
+    vector <int> nums = {1, 2, -3, 0, -4, -5};
+    cout << "The max subproduct is " << maxProductSubArray(nums);
+
+
+
+
+    return 0;
+}
