8be2e8f547b6cad0238acf15ae372b3affe18b1b
Vedant Bhagyawant <maharaja@e.gov>
2024-07-21 11:00:29 +0530

    PYQ2

---
+++ Array/Accenture/pyq2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int oddeven(int arr[],int size){
+    vector<int> even;
+    vector<int> odd;
+
+
+    for(int i = 0; i <= size-1; i++){
+        if(i % 2 == 0){
+            even.push_back(arr[i]); 
+        }
+        else{
+            odd.push_back(arr[i]);
+
+        }
+    }
+    
+    sort(even.begin(),even.end());
+    sort(odd.begin(),odd.end());
+
+    int value = even[1] + odd[1];
+
+    return value;
+    
+
+       
+}
+
+int main(){
+    int arr[] = {3,2,1,7,5,4};
+    int size = sizeof(arr)/ sizeof(arr[0]);
+
+    int output = oddeven(arr,size);
+    cout<< "The adding of the second largest and second smallest element is "<< output <<endl;
+
+
+
+    return 0;
+}
